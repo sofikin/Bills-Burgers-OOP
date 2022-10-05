b41594db185d65f6e9b99f4f5e0acf3ce36ca0d5
Sofia Kalpin <sofia.kalpin@khk.ee>
2022-10-05 23:27:50 +0300

    "Healthy Hamburger class"

---
+++ src/HealthyBurger.java
@@ -0,0 +1,34 @@
+public class HealthyBurger extends Hamburger{
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String meat, double price) {
+        super("Healthy Burger", meat, "Brown rye roll", price);
+    }
+
+    public void addHealthyAddition1(String name, double price) {
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+    }
+
+    public void addHealthyAddition2(String name, double price) {
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+    }
+
+    @Override
+    public double itemizeHamburger() {
+        double totalPrice = super.itemizeHamburger();
+        if (healthyExtra1Name != null){
+            System.out.println("Added " + this.healthyExtra1Name + " for an extra " + this.healthyExtra1Price);
+            totalPrice += this.healthyExtra1Price;
+        }
+        if (healthyExtra2Name != null){
+            System.out.println("Added " + this.healthyExtra2Name + " for an extra " + this.healthyExtra2Price);
+            totalPrice += this.healthyExtra2Price;
+        }
+        return totalPrice;
+    }
+}

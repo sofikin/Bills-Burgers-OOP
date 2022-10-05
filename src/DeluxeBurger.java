83be03742bc4449d6de132edc61a0dcb87dd5068
Sofia Kalpin <sofia.kalpin@khk.ee>
2022-10-05 23:03:43 +0300

    "Deluxe Hamburger initial file"

--- src/DeluxeBurger.java
+++ src/DeluxeBurger.java
@@ -3,6 +3,26 @@ public class DeluxeBurger extends Hamburger {
         super("Deluxe Hamburger", "Sausage & Bacon", "White roll", 14.54);
         super.addHamburgerAddition1("Chips", 2.74);
         super.addHamburgerAddition2("Drink", 1.81);
+
+
     }
 
+    @Override
+    public void addHamburgerAddition1(String additionName, double additionPrice) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+    @Override
+    public void addHamburgerAddition2(String additionName, double additionPrice) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String additionName, double additionPrice) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String additionName, double additionPrice) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
 }
